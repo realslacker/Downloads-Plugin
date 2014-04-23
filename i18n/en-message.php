d443ebb151fcf706ccd1df40b56b43926ccc65f3
seven <seven@geovoyagers.de>
2014-04-23 22:52:57 +0200

    Create en-message.php
    Note: Removed all occurrences of ":"

---
+++ i18n/en-message.php
@@ -0,0 +1,61 @@
+<?php
+/**
+* English language file for download manager
+* Tina Keil, geovoyagers.de
+* 
+* @package wolf
+* @subpackage Downloads
+*
+**/
+return array(
+	'Download Manager' => 'Download Manager',
+	'Provides interface to manage downloadable files.' => 'Provides interface to manage downloadable files.',
+	'Download Manager Plugin' => 'Download Manager Plugin',
+	'Name' => 'Name',
+	'ID' => 'ID',
+	'Downloads' => 'Downloads',
+	'Created' => 'Created',
+	'Expires' => 'Expires',
+	'Action' => 'Action',
+	'List' => 'List',
+	'never' => 'never',
+	'Add New' => 'Add New',
+	'Documentation' => 'Documentation',
+	'Plugin Version' => 'Plugin Version',
+	'Usage' => 'Usage',
+	'Edit Download' => 'Edit Download',
+	'New Download' => 'New Download',
+	'File' => 'File',
+	'Download' => 'Download',
+	'Allowed filetypes' => 'Allowed filetypes',
+	'Information' => 'Information',
+	'Description' => 'Description',
+	'The download name; be descriptive' => 'The download name; be descriptive',
+	'Describe the download; visible to the public' => 'Describe the download; visible to the public',
+	'Key Words' => 'Key Words',
+	'Key words for search; separate with spaces' => 'Key words for search; separate with spaces',
+	'Tags' => 'Tags',
+	'Seperate with commas' => 'Seperate with commas',
+	'Expires Date' => 'Expires Date',
+	'Use MM/DD/YYYY format' => 'Use MM/DD/YYYY format',
+	'Active' => 'Active',
+	'Is the download active?' => 'Is the download active?',
+	'Save' => 'Save',
+	'You have modified this page. If you navigate away from this page without first saving your data, the changes will be lost.' => 'You have modified this page. If you navigate away from this page without first saving your data, the changes will be lost.',
+	'Download Manager Plugin Settings' => 'Download Manager Plugin Settings',
+	'Paths' => 'Paths',
+	'Downloads Path' => 'Downloads Path',
+	'System path relative to CMS_ROOT' => 'System path relative to CMS_ROOT',
+	'Downloads URI' => 'Downloads URI',
+	'URI relative to URI_PUBLIC' => 'URI relative to URI_PUBLIC',
+	'Upload Controls' => 'Upload Controls',
+	'Allowed file extensions' => 'Allowed file extensions',
+	'File Creation Defaults' => 'File Creation Defaults',
+	'Umask' => 'Umask',
+	'Default PHP umask; see <a href="http://php.net/manual/en/function.umask.php">umask()</a>' => 'Default PHP umask; see <a href="http://php.net/manual/en/function.umask.php">umask()</a>',
+	'Directory Creation Mode' => 'Directory Creation Mode',
+	'Default PHP directory creation mode; see <a href="http://php.net/manual/en/function.chmod.php">chmod()</a>' => 'Default PHP directory creation mode; see <a href="http://php.net/manual/en/function.chmod.php">chmod()</a>',
+	'File Creation Mode' => 'File Creation Mode',
+	'Default PHP file creation mode; see <a href="http://php.net/manual/en/function.chmod.php">chmod()</a>' => 'Default PHP file creation mode; see <a href="http://php.net/manual/en/function.chmod.php">chmod()</a>',
+
+);
